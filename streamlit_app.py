fcd0f5fd21c36835d118999f733e623dc3a90d76
Naresh Mogilipalem <naresh.jfx@gmail.com>
2025-12-28 20:02:17 -0500

    Update streamlit_app.py
    Added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,12 @@ ctx = st.connection("snowflake");
 session = ctx.session()
 
 # session = get_active_session()
-df = session.table("smoothies.public.fruit_options").select("fruit_name")
+df = session.table("smoothies.public.fruit_options").select("fruit_name", "search_on")
 # st.dataframe(data=df, use_container_width=True)
+pd_df = df.to_pandas();
+st.dataframe(pd_df)
+st.stop()
+
 
 name_on_order = st.text_input("Name on smoothies");
 
