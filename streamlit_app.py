43feb85fcee584c9eab9316a2157a709a21359cd
Naresh Mogilipalem <naresh.jfx@gmail.com>
2025-12-28 19:35:49 -0500

    Update streamlit_app.py
    Updated ingradient stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie!:cup_with_straw")
@@ -29,6 +30,9 @@ if ingredient_list:
     
     for x in ingredient_list:
         ingredients_string += x + " "
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + x)
+        sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
+      
     
     st.write(ingredients_string)
     
@@ -43,7 +47,5 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         st.write("Your smoothies is ordered")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
+
+
